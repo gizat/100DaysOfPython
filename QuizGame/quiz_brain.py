f32f6b977df8ac27d7f6152a076fccc2e7a2b4be
Gizat <gizat.makhanov@gmail.com>
2021-02-25 08:43:11 +0600

    First commit of the Quiz game

---
+++ QuizGame/quiz_brain.py
@@ -0,0 +1,11 @@
+class QuizBrain():
+
+    def __init__(self, question_list):
+        self.question_number = 0
+        self.question_list = question_list
+
+
+    
+# TODO: asking new questions
+# TODO: checking if the answer was correct
+# TODO: checking if the end of the quiz
