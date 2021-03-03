527c9933fe61a5458bf74f5974d8cd0df3fd2066
Gizat <gizat.makhanov@gmail.com>
2021-03-03 13:59:59 +0600

    Created a car class

--- TurtleCrossingGame/car_manager.py
+++ TurtleCrossingGame/car_manager.py
@@ -1,7 +1,36 @@
+from turtle import Turtle
+import random
+
 COLORS = ["red", "orange", "yellow", "green", "blue", "purple"]
 STARTING_MOVE_DISTANCE = 5
 MOVE_INCREMENT = 10
+STRETCH_WID = 2
+STRETCH_LEN = 1
+STARTING_X = 300
+
+class CarManager(Turtle):
+    
+    def __init__(self):
+        super().__init__()
+        self.hideturtle()
+        self.cars = []
 
 
-class CarManager:
-    pass
+    def add_car(self):
+        new_car = Turtle('square')
+        new_car.color(random.choice(COLORS))
+        new_car.penup()
+        new_car.shapesize(stretch_wid=1, stretch_len=2)
+        new_car.y_cor = self.random_pos()
+        new_car.goto(STARTING_X, new_car.y_cor)
+        self.cars.append(new_car)
+
+
+    def random_pos(self):
+        return random.randint(-250, 250)
+
+
+    def move(self):
+        for car in self.cars:
+            new_x = car.xcor() - MOVE_INCREMENT
+            car.goto(new_x, car.y_cor)
--- TurtleCrossingGame/main.py
+++ TurtleCrossingGame/main.py
@@ -10,16 +10,25 @@ screen.title('Turtle Crossing Game')
 screen.tracer(0)
 
 turtle = Player()
+cars = CarManager()
 
 screen.listen()
 screen.onkey(turtle.up, 'Up')
 
 game_is_on = True
+counter = 0
 while game_is_on:
     time.sleep(0.1)
     screen.update()
+    
+    if counter%6 == 0:
+        cars.add_car()
+    
+    cars.move()
+    counter += 1
 
-# TODO: Create cars that are 20px high by 40px wide 
+
+
 # TODO: Detect when the turtle player collides with a car
 # TODO: Detect when the turtle player has reached the top edge of the screen
 # TODO: Create a scoreboard
