9f2a719ee4a7b1a1f16fdcfe63a19fc66c486b12
Gizat <gizat.makhanov@gmail.com>
2021-03-17 00:58:01 +0600

    Send random quotes via email

---
+++ BirthdayWisherSMTPLIB/quoteSender.py
@@ -0,0 +1,22 @@
+import datetime as dt
+from random import choice
+import smtplib
+
+my_email = "fryphilip99@yahoo.com"
+password = "foalcvfybucuxgrf"
+
+now = dt.datetime.now()
+weekday = now.weekday()
+if weekday == 2:
+    with open("quotes.txt") as quote_file:
+        all_quotes = quote_file.readlines()
+        quote = choice(all_quotes)
+    
+    with smtplib.SMTP("smtp.mail.yahoo.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=password)
+        connection.sendmail(
+            from_addr=my_email, 
+            to_addrs="gizat.makhanov@gmail.com", 
+            msg=f"Subject:Hello\n\n{quote}"
+        )
