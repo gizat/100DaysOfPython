ec13a2ff29981f4b7753f3df8118c731c344d5ea
Gizat <gizat.makhanov@gmail.com>
2021-03-20 05:24:02 +0600

    Created Kanye quote generator using API

---
+++ ISSoverheadAPI/main.py
@@ -0,0 +1,13 @@
+import requests
+
+response = requests.get(url="http://api.open-notify.org/iss-now.json")
+response.raise_for_status()
+
+data = response.json()
+
+lat = data["iss_position"]["latitude"]
+lon = data["iss_position"]["longitude"]
+
+iss_position = (lon, lat)
+
+print(iss_position)
