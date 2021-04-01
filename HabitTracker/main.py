37a04dc97cc3e9fed9dc36f39dd61dd8ab4f189e
Gizat <gizat.makhanov@gmail.com>
2021-04-01 07:20:37 +0600

    Add a pixel

--- HabitTracker/main.py
+++ HabitTracker/main.py
@@ -1,20 +1,55 @@
 import requests
+import os
+from dotenv import load_dotenv
+import datetime
 
-pixela_endpoint = "https://pixe.la/v1/users"
+load_dotenv()  # take environment variables from .env.
+
+PIXELA_ENDPOINT = "https://pixe.la/v1/users"
 USERNAME = "gizat"
+TOKEN = os.environ.get("PIXELA_TOKEN")
 
-user_params = {
-    "token": "PRkpGkF8E66ib3hyTbQbBm483STGuy",
-    "username": USERNAME,
-    "agreeTermsOfService": "yes",
-    "notMinor": "yes"
+headers = {
+    "X-USER-TOKEN": TOKEN
 }
 
-# response = requests.post(url=pixela_endpoint, json=user_params)
+### ADD A PIXEL
+
+graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs/graph1"
+
+today = datetime.datetime.now().strftime("%Y%m%d")
+quantity = 4
+
+pixel_params = {
+    "date": str(today),
+    "quantity": str(quantity)
+}
+
+response = requests.post(url=graph_endpoint, headers=headers, json=pixel_params)
+print(response.text)
+
+
+### CREATE A GRAPH 
+
+# graph_endpoint = f"{PIXELA_ENDPOINT}/{USERNAME}/graphs"
+
+# graph_config = {
+#     "id": "graph1",
+#     "name": "Coding Graph",
+#     "unit": "pomodoro",
+#     "type": "int",
+#     "color": "shibafu"
+# }
+
+# response = requests.post(url=graph_endpoint, json=graph_config, headers=headers)
 # print(response.text)
 
-graph_endpoint = f"{pixela_endpoint}/{USERNAME}/graphs"
 
-graph_config = {
-    "id"
-}
+### CREATE A USER
+
+# user_params = {
+#     "token": TOKEN,
+#     "username": USERNAME,
+#     "agreeTermsOfService": "yes",
+#     "notMinor": "yes"
+# }
