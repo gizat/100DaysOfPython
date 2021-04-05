95626935224234fb15389b309a9badca91bf0809
Gizat <gizat.makhanov@gmail.com>
2021-04-05 18:26:58 +0600

    Created flight deals search app

--- FlightDealFinder/flight_search.py
+++ FlightDealFinder/flight_search.py
@@ -1,3 +1,70 @@
+import requests
+import os
+from dotenv import load_dotenv
+from flight_data import FlightData
+
+load_dotenv()  # take environment variables from .env.
+
 class FlightSearch:
-    #This class is responsible for talking to the Flight Search API.
-    pass
+    
+    def __init__(self):
+        self.code = "TESTING"
+        self.tequilla_api = os.environ.get("TEQUILLA_API")
+
+    def get_destination_code(self, city_name):
+        tequilla_ep = "https://tequila-api.kiwi.com/locations/query"
+        
+        headers = {
+            "apikey":self.tequilla_api
+        }
+        params = {
+            "term":city_name,
+            "location_types": "city"
+        }
+
+        response = requests.get(url=tequilla_ep, params=params, headers=headers)
+        result = response.json()["locations"]
+        self.code = result[0]["code"]
+
+        return self.code
+
+
+    def search_flights(self, origin, destination, from_time, to_time):
+        deals_ep = "https://tequila-api.kiwi.com/v2/search"
+
+        headers = {
+            "apikey":self.tequilla_api
+        }
+
+        query = {
+            "fly_from": origin,
+            "fly_to": destination,
+            "date_from": from_time.strftime("%d/%m/%Y"),
+            "date_to": to_time.strftime("%d/%m/%Y"),
+            "nights_in_dst_from": 7,
+            "nights_in_dst_to": 28,
+            "flight_type": "round",
+            "one_for_city": 1,
+            "max_stopovers": 0,
+            "curr": "GBP"
+        }
+
+        response = requests.get(url=deals_ep, params=query, headers=headers)
+
+        try:
+            data = response.json()["data"][0]
+        except IndexError:
+            print(f"No flights found for {destination}.")
+            return None
+
+        flight_data = FlightData(
+            price=data["price"],
+            origin_city=data["route"][0]["cityFrom"],
+            origin_airport=data["route"][0]["flyFrom"],
+            destination_city=data["route"][0]["cityTo"],
+            destination_airport=data["route"][0]["flyTo"],
+            out_date=data["route"][0]["local_departure"].split("T")[0],
+            return_date=data["route"][1]["local_departure"].split("T")[0]
+        )
+        print(f"{flight_data.destination_city}: £{flight_data.price}")
+        return flight_data
--- FlightDealFinder/main.py
+++ FlightDealFinder/main.py
@@ -1,7 +1,31 @@
-import requests
-import os
-from dotenv import load_dotenv
-from datetime import datetime
+from pprint import pprint
+from data_manager import DataManager
+from flight_search import FlightSearch
+from datetime import datetime, timedelta
 
-load_dotenv()  # take environment variables from .env.
+data_manager = DataManager()
+flight_search = FlightSearch()
 
+sheet_data = data_manager.get_destination_data()
+# pprint(sheet_data)
+
+ORIGIN_CITY_IATA = "TSE"
+
+if sheet_data[0]["iataCode"] == "":
+    for row in sheet_data:
+        row["iataCode"] = flight_search.get_destination_code(row["city"])
+    # print(f"sheet_data:\n {sheet_data}")
+
+    data_manager.destination_data = sheet_data
+    data_manager.update_destination_codes()
+
+tomorrow = datetime.now() + timedelta(days=1)
+six_month_from_today = datetime.now() + timedelta(days=(6 * 30))
+
+for destination in sheet_data:
+    flight = flight_search.search_flights(
+        ORIGIN_CITY_IATA,
+        destination["iataCode"],
+        from_time=tomorrow,
+        to_time=six_month_from_today
+    )
