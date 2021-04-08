78e54f59b4f808bde793d34dc71251e12462c165
Gizat <gizat.makhanov@gmail.com>
2021-04-09 01:19:02 +0600

    Scraped 100 best movies

--- ScrapingData/movies100.py
+++ ScrapingData/movies100.py
@@ -2,11 +2,15 @@ from bs4 import BeautifulSoup
 import requests
 
 
-response = requests.get(url="https://www.empireonline.com/movies/features/best-movies-2")
+response = requests.get(url="https://www.hollywoodreporter.com/lists/100-best-films-ever-hollywood-favorites-818512")
 empire_webpage = response.text
 
 soup = BeautifulSoup(empire_webpage, "html.parser")
 
-print(soup.prettify())
-movies = soup.find_all(name="h3", class_="jsx-4245974604")
-print(movies)
+all_movies = soup.find_all(name="h1", class_="list-item__title")
+movies = [movie.getText() for movie in all_movies]
+movie_titles = movies[::-1]
+
+with open('movies.txt', mode='w') as file:
+    for i in range(len(movie_titles)):
+        contents = file.write(f"{i+1}) {movie_titles[i]}\n")
