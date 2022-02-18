ff794539e5238ec1d8ff077a1cfe79a83030fb9f
gizat <gizat.makhanov@gmail.com>
2022-02-18 23:53:54 +0600

    Storing CSS elements inside a dictionary

--- CookieClicker/main.py
+++ CookieClicker/main.py
@@ -3,6 +3,7 @@ from selenium.webdriver.chrome.service import Service as ChromeService
 from selenium.webdriver.common.by import By
 from selenium.webdriver.common.keys import Keys
 import time
+import re
 
 service = ChromeService(executable_path="/opt/homebrew/bin/chromedriver")
 driver = webdriver.Chrome(service=service)
@@ -11,8 +12,10 @@ cookie_clicker_url = "https://orteil.dashnet.org/experiments/cookie/"
 driver.get(cookie_clicker_url)
 cookie_button = driver.find_element(By.CSS_SELECTOR, "#cookie")
 
-store_elements = driver.find_elements(By.CSS_SELECTOR, "#store div")
+store_elements = driver.find_elements(By.CSS_SELECTOR, "#store div:not(class)")
 ids = {element.get_attribute("id"):0 for element in store_elements}
+ids.popitem()
+print(ids)
 
 timeout = time.time() + 5
 five_min = time.time() + 300
@@ -21,11 +24,17 @@ while True:
     cookie_button.click()
 
     if time.time() > timeout:
-        all_prices = driver.find_elements(By.CSS_SELECTOR, "#store b")
+        prices_raw = driver.find_elements(By.CSS_SELECTOR, "#store div:not([class]) b")
+        prices_raw.pop()
+        prices = []
 
-        
-        for element in ids:
-            element.update()
+        for price in prices_raw:
+            prices.append(int(re.sub(",", "", price.text.split("-")[1].strip())))
+
+        i = 0
+        for key in ids.keys():
+            ids[key] = prices[i]
+            i += 1
 
         timeout = time.time() + 5
 
